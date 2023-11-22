c338eb57ba8734c307231ba13224790dee6a9cde
Gabriel Darnok <silva0349@gmail.com>
2023-11-21 21:07:07 -0300

    email de confirmação após o pagamento

---
+++ blog/resources/views/emails/pagamento.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagamento</title>
+</head>
+<body>
+    <p>Olá, {{ $nome }}!</p>
+    
+    <p>Pagamento efetuado com sucesso, no valor de: R$ {{ $valor }}.</p>
+
+</body>
+</html>
